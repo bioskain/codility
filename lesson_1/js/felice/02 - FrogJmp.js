fc0f66321d39777e547b7cf846aabfe9e72f2043
Felice Pagano <felice.pagano@unionefiduciaria.it>
2015-10-14 11:20:30 +0200

    adds exercise 2 of module 1

---
+++ lesson_1/js/felice/02 - FrogJmp.js
@@ -0,0 +1,9 @@
+// https://codility.com/demo/results/trainingSYVK3U-ZBD/
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function solution(X, Y, D) {
+    // write your code in JavaScript (Node.js 4.0.0)
+    
+    return Math.ceil((Y-X)/D);
+}
